0d84824f8733f242e8e0f8eda3a3afceb427beea
ross <rossmere67@gmail.com>
2022-08-09 21:57:24 +0100

    started json editor

--- frontend/src/components/ui.js
+++ frontend/src/components/ui.js
@@ -1,9 +1,11 @@
 import { Box } from 'theme-ui';
 import PropTypes from 'prop-types';
-import { useContext, useRef } from 'react';
+import { useContext, useRef, useState } from 'react';
+import isObject from 'lodash.isobject';
 import propTypes from '../propTypes';
 import { AdaptContext } from './contexts';
 import RecursiveComponentTree from './componentTree';
+import CaretRight from '../icons/caretRight.svg';
 
 function ComponentTree(element) {
   if (!element?.components?.length) {
@@ -51,6 +53,86 @@ function getComponentTreeRefs(componentTree) {
   return refs;
 }
 
+// [
+//   {
+//     key: 'background',
+//     value: 'red',
+//   },
+//   {
+//     key: 'color',
+//     value: ['red', 'yellow'],
+//   },
+// ];
+
+function CollapseButton({ open, setOpen }) {
+  const sx = {
+    cursor: 'pointer',
+  };
+  if (open) {
+    sx.transform = 'rotate(90deg)';
+  }
+  return <CaretRight onClick={() => setOpen(!open)} style={sx} />;
+}
+
+function CollaspableNode({ path, tree }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: open ? 'column' : 'row' }}>
+      <Box sx={{ whiteSpace: 'pre-wrap' }}>
+        <CollapseButton open={open} setOpen={setOpen} />
+        {tree.key}
+        :
+        {'   '}
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: open ? 'column' : 'row',
+          ml: open ? 4 : 0,
+        }}
+      >
+        <JSONEditor path={path} tree={tree.value} />
+      </Box>
+    </Box>
+  );
+}
+
+function JSONEditor({ path = [], tree }) {
+  const parentLevel = path.length ? path[path.length - 1][0] : -1;
+
+  if (Array.isArray(tree)) {
+    return tree.map((node, index) => (
+      <JSONEditor
+        key={JSON.stringify([...path, [parentLevel + 1, index]])}
+        path={[...path, [parentLevel + 1, index]]}
+        tree={node}
+      />
+    ));
+  }
+
+  if (Array.isArray(tree?.value)) {
+    if (Array.isArray(tree.value)) {
+      return <CollaspableNode path={path} tree={tree} />;
+    }
+  }
+
+  if (tree?.value !== undefined) {
+    return (
+      <Box sx={{ display: 'flex' }}>
+        <Box sx={{ whiteSpace: 'pre-wrap' }}>
+          {tree.key}
+          :
+          {'   '}
+        </Box>
+        <Box>{tree.value}</Box>
+      </Box>
+    );
+  }
+
+  return <div>{tree}</div>;
+}
+
 function Ui({ sx }) {
   const { elements, elementInView } = useContext(AdaptContext);
   const componentRefs = useRef();
@@ -62,9 +144,45 @@ function Ui({ sx }) {
     return <Box sx={{ flex: 1, ...sx }}>No element selected to view</Box>;
   }
 
+  const tree = [
+    {
+      key: 'background',
+      value: ['red', 'green', 'yellow', 'pink'],
+    },
+    {
+      key: 'color',
+      value: 'yellow',
+    },
+    {
+      key: 'h1',
+      value: [
+        {
+          key: 'backgroundColor',
+          value: 'tomato',
+        },
+        {
+          key: 'a, span',
+          value: [
+            {
+              key: 'backgroundColor',
+              value: 'white',
+            },
+            {
+              key: 'color',
+              value: 'tomato',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
   return (
     <Box sx={{ flex: 1, ...sx }}>
       <ComponentTree tree={elements[elementInView]} />
+      <Box>
+        <JSONEditor tree={tree} />
+      </Box>
     </Box>
   );
 }
