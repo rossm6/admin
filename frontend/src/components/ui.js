3ff02706c502f4003e95f776df710346c0fcf817
ross <rossmere67@gmail.com>
2022-08-13 22:21:43 +0100

    Further json widget

--- frontend/src/components/ui.js
+++ frontend/src/components/ui.js
@@ -1,11 +1,17 @@
-import { Box } from 'theme-ui';
-import PropTypes from 'prop-types';
-import { useContext, useRef, useState } from 'react';
+import { Box, Button, IconButton } from 'theme-ui';
+import PropTypes, { element } from 'prop-types';
+import {
+  useCallback, useContext, useEffect, useRef, useState,
+} from 'react';
 import isObject from 'lodash.isobject';
+import clone from 'lodash.clone';
+import { isString } from 'lodash';
 import propTypes from '../propTypes';
 import { AdaptContext } from './contexts';
 import RecursiveComponentTree from './componentTree';
 import CaretRight from '../icons/caretRight.svg';
+import PlusCircleFill from '../icons/plusCircleFill.svg';
+import PencilFill from '../icons/pencilFill.svg';
 
 function ComponentTree(element) {
   if (!element?.components?.length) {
@@ -53,34 +59,63 @@ function getComponentTreeRefs(componentTree) {
   return refs;
 }
 
-// [
-//   {
-//     key: 'background',
-//     value: 'red',
-//   },
-//   {
-//     key: 'color',
-//     value: ['red', 'yellow'],
-//   },
-// ];
-
-function CollapseButton({ open, setOpen }) {
+function CollapseButton({ expandNode, path, tree }) {
   const sx = {
     cursor: 'pointer',
   };
-  if (open) {
+  if (tree.expand) {
     sx.transform = 'rotate(90deg)';
   }
-  return <CaretRight onClick={() => setOpen(!open)} style={sx} />;
+  return <CaretRight onClick={() => expandNode(path, !tree.expand)} style={sx} />;
 }
 
-function CollaspableNode({ path, tree }) {
-  const [open, setOpen] = useState(false);
+function getNodeAtPath(treeData, path, childrenKey = 'value') {
+  const point = path[0];
+  if (path.length > 1) {
+    return getNodeAtPath(treeData[point[1]][childrenKey], path.slice(1, undefined));
+  }
+  return treeData[point[1]];
+}
+
+function* traverseTree(tree) {
+  if (Array.isArray(tree)) {
+    for (let i = 0; i < tree.length; i++) {
+      yield* traverseTree(tree[i]);
+    }
+  } else if (Array.isArray(tree?.value)) {
+    yield tree;
+    yield* traverseTree(tree.value);
+  } else if (tree?.value !== undefined) {
+    yield tree;
+  } else {
+    yield tree;
+  }
+}
+
+function CollaspableNode({
+  expandNode,
+  path,
+  tree,
+  wholeTree,
+  onChange,
+  onLosingFocus,
+  edit,
+  parentCollapsableNodeIsExpanded,
+  addPropToValue,
+  elementToFocusOn,
+}) {
+  let showButton = true;
+  if (path.length > 1) {
+    const node = getNodeAtPath(wholeTree, path.slice(-2, -1));
+    if (!node.expand) {
+      showButton = false;
+    }
+  }
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: open ? 'column' : 'row' }}>
+    <Box sx={{ display: 'flex', flexDirection: tree.expand ? 'column' : 'row' }}>
       <Box sx={{ whiteSpace: 'pre-wrap' }}>
-        <CollapseButton open={open} setOpen={setOpen} />
+        {showButton && <CollapseButton expandNode={expandNode} tree={tree} path={path} />}
         {tree.key}
         :
         {'   '}
@@ -88,49 +123,241 @@ function CollaspableNode({ path, tree }) {
       <Box
         sx={{
           display: 'flex',
-          flexDirection: open ? 'column' : 'row',
-          ml: open ? 4 : 0,
+          flexDirection: tree.expand ? 'column' : 'row',
+          ml: tree.expand ? 4 : 0,
         }}
       >
-        <JSONEditor path={path} tree={tree.value} />
+        <JSONEditorComponent
+          expandNode={expandNode}
+          path={path}
+          tree={tree.value}
+          wholeTree={wholeTree}
+          onChange={onChange}
+          onLosingFocus={onLosingFocus}
+          edit={edit}
+          parentCollapsableNodeIsExpanded={parentCollapsableNodeIsExpanded && !!tree.expand}
+          addPropToValue={addPropToValue}
+          elementToFocusOn={elementToFocusOn}
+        />
       </Box>
     </Box>
   );
 }
 
-function JSONEditor({ path = [], tree }) {
+function JSONEditorComponent({
+  expandNode,
+  path = [],
+  tree,
+  wholeTree,
+  onChange,
+  onLosingFocus,
+  edit,
+  parentCollapsableNodeIsExpanded = true,
+  addPropToValue,
+  elementToFocusOn,
+}) {
   const parentLevel = path.length ? path[path.length - 1][0] : -1;
 
   if (Array.isArray(tree)) {
-    return tree.map((node, index) => (
-      <JSONEditor
-        key={JSON.stringify([...path, [parentLevel + 1, index]])}
-        path={[...path, [parentLevel + 1, index]]}
-        tree={node}
-      />
-    ));
+    return (
+      <>
+        {tree.map((node, index) => (
+          <JSONEditorComponent
+            expandNode={expandNode}
+            key={JSON.stringify([...path, [parentLevel + 1, index]])}
+            path={[...path, [parentLevel + 1, index]]}
+            tree={node}
+            wholeTree={wholeTree}
+            onChange={onChange}
+            onLosingFocus={onLosingFocus}
+            edit={edit}
+            parentCollapsableNodeIsExpanded={parentCollapsableNodeIsExpanded}
+            addPropToValue={addPropToValue}
+            elementToFocusOn={elementToFocusOn}
+          />
+        ))}
+        {parentCollapsableNodeIsExpanded && (
+          <IconButton
+            onClick={() => addPropToValue(path)}
+            sx={{ color: 'blue', cursor: 'pointer' }}
+          >
+            <PlusCircleFill />
+          </IconButton>
+        )}
+      </>
+    );
   }
 
   if (Array.isArray(tree?.value)) {
-    if (Array.isArray(tree.value)) {
-      return <CollaspableNode path={path} tree={tree} />;
-    }
+    return (
+      <CollaspableNode
+        expandNode={expandNode}
+        path={path}
+        tree={tree}
+        wholeTree={wholeTree}
+        onChange={onChange}
+        onLosingFocus={onLosingFocus}
+        edit={edit}
+        parentCollapsableNodeIsExpanded={parentCollapsableNodeIsExpanded}
+        addPropToValue={addPropToValue}
+        elementToFocusOn={elementToFocusOn}
+      />
+    );
   }
 
-  if (tree?.value !== undefined) {
+  if (!tree?.cssValue) {
     return (
       <Box sx={{ display: 'flex' }}>
-        <Box sx={{ whiteSpace: 'pre-wrap' }}>
-          {tree.key}
-          :
-          {'   '}
-        </Box>
-        <Box>{tree.value}</Box>
+        {tree.editKey ? (
+          <textarea
+            onChange={(e) => {
+              onChange(path, e.target.value);
+            }}
+            onBlur={() => onLosingFocus(path, 'key')}
+            value={tree.key}
+          />
+        ) : (
+          <Box sx={{ whiteSpace: 'pre-wrap' }}>
+            {tree.key}
+            :
+            {'   '}
+          </Box>
+        )}
+        {tree.editValue ? (
+          <textarea
+            onChange={(e) => {
+              onChange(path, e.target.value);
+            }}
+            onBlur={() => onLosingFocus(path, 'value')}
+            value={tree.value}
+          />
+        ) : (
+          <Box>{tree.value.value}</Box>
+        )}
+        {parentCollapsableNodeIsExpanded && (
+          <IconButton>
+            <PlusCircleFill onClick={() => addPropToValue(path)} />
+          </IconButton>
+        )}
       </Box>
     );
   }
 
-  return <div>{tree}</div>;
+  return tree?.edit ? (
+    <textarea
+      onChange={(e) => onChange(path, e.target.value)}
+      ref={elementToFocusOn}
+      value={tree?.value}
+      style={{
+        display: tree?.edit ? 'block' : 'none',
+      }}
+    />
+  ) : (
+    <div onClick={() => edit(path)}>{tree?.value}</div>
+  );
+}
+
+function JSONEditor({ tree }) {
+  const [treeData, setTreeData] = useState(tree);
+  const elementToFocusOn = useRef();
+
+  useEffect(() => {
+    if (elementToFocusOn.current) {
+      console.log(elementToFocusOn);
+      elementToFocusOn.current.focus();
+    }
+  });
+
+  const expandNode = useCallback(
+    (path, expand) => {
+      const copy = clone(treeData);
+      // traverse the tree
+      const node = getNodeAtPath(copy, path);
+      node.expand = expand;
+      for (const childNode of traverseTree(node.value)) {
+        if (childNode.expand) {
+          childNode.expand = false;
+        }
+      }
+      setTreeData(copy);
+    },
+    [treeData, setTreeData],
+  );
+
+  const onChange = useCallback(
+    (path, value, keyToChange = 'value') => {
+      const copy = clone(treeData);
+      const node = getNodeAtPath(copy, path);
+      node[keyToChange] = value;
+      setTreeData(copy);
+    },
+    [treeData, setTreeData],
+  );
+
+  const edit = useCallback(
+    (path) => {
+      const copy = clone(treeData);
+      const node = getNodeAtPath(copy, path);
+      node.edit = true;
+      setTreeData(copy);
+    },
+    [treeData, setTreeData],
+  );
+
+  const addPropToValue = useCallback(
+    (path) => {
+      if (path.length) {
+        const copy = clone(treeData);
+        const node = getNodeAtPath(copy, path);
+        if (node?.value?.cssValue) {
+          node.value = [
+            node.value,
+            {
+              cssValue: true,
+              value: '...',
+              edit: false,
+            },
+          ];
+        } else if (Array.isArray(node.value)) {
+          node.value = [
+            ...node.value,
+            {
+              cssValue: true,
+              value: '...',
+              edit: false,
+            },
+          ];
+        }
+        setTreeData(copy);
+      } else {
+        setTreeData([
+          ...treeData,
+          {
+            key: '',
+            value: {
+              cssValue: true,
+              value: '',
+              edit: false,
+            },
+          },
+        ]);
+      }
+    },
+    [treeData, setTreeData],
+  );
+
+  return (
+    <JSONEditorComponent
+      edit={edit}
+      expandNode={expandNode}
+      onChange={onChange}
+      onLosingFocus={onLosingFocus}
+      tree={treeData}
+      wholeTree={treeData}
+      addPropToValue={addPropToValue}
+      elementToFocusOn={elementToFocusOn}
+    />
+  );
 }
 
 function Ui({ sx }) {
@@ -147,29 +374,66 @@ function Ui({ sx }) {
   const tree = [
     {
       key: 'background',
-      value: ['red', 'green', 'yellow', 'pink'],
+      value: [
+        {
+          cssValue: true,
+          value: 'red',
+          edit: false,
+        },
+        {
+          cssValue: true,
+          value: 'pink',
+          edit: false,
+        },
+        {
+          cssValue: true,
+          value: 'yellow',
+          edit: false,
+        },
+        {
+          cssValue: true,
+          value: 'green',
+          edit: false,
+        },
+      ],
     },
     {
       key: 'color',
-      value: 'yellow',
+      value: {
+        cssValue: true,
+        value: 'yellow',
+        edit: false,
+      },
     },
     {
       key: 'h1',
       value: [
         {
           key: 'backgroundColor',
-          value: 'tomato',
+          value: {
+            cssValue: true,
+            value: 'yellow',
+            edit: false,
+          },
         },
         {
           key: 'a, span',
           value: [
             {
               key: 'backgroundColor',
-              value: 'white',
+              value: {
+                cssValue: true,
+                value: 'red',
+                edit: false,
+              },
             },
             {
               key: 'color',
-              value: 'tomato',
+              value: {
+                cssValue: true,
+                value: 'pink',
+                edit: false,
+              },
             },
           ],
         },
